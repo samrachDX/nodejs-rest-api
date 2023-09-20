582244da49565d464b3b23b48f67557f7d31863d
samrachDXlocal <samraichchan@gmail.com>
2023-09-20 18:55:44 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// app.js
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello, World!' });
+});
+
+app.post('/data', (req, res) => {
+  const { body } = req;
+  res.json({ receivedData: body });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
